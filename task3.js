a8148e8120ae7ba25066f239072699bdc59a7fd3
Makarim Muhammad <97607349+makarim22@users.noreply.github.com>
2025-12-30 10:59:24 +0700

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,24 @@
+const arr = [1, 3, 5, 7, 8, 9, 6, 2, 4, 10];
+
+
+function sort(array) {
+    let sorted = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        sorted[i] = array[i];
+    }
+
+    for (let i = 0; i < sorted.length - 1; i++) {
+        for (let j = 0; j < sorted.length - 1 - i; j++) {
+            if (sorted[j] > sorted[j + 1]) {
+                let temp = sorted[j];
+                sorted[j] = sorted[j + 1];
+                sorted[j + 1] = temp;
+            }
+        }
+    }
+    
+    return sorted;
+}
+console.log("hello");
+console.log(sort(arr));
